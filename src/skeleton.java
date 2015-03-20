e860be92996c40896668cbc6e33c19f2b96e2ba8
conicole <corentin.nicol@live.com>
2015-03-20 12:52:38 +0100

    Create first ask message for skeleton

--- src/skeleton.java
+++ src/skeleton.java
@@ -1,10 +1,61 @@
+import java.util.Scanner;
 /**
  * Created by Nicole on 19/03/2015.
  */
 public class skeleton {
 
+    public static void writeLine(){
+        System.out.println("*************");
+    }
     public static void main(String[] args){
-       System.out.println("Hello World");
+        writeLine();
+        System.out.println("Skeleton Program ");
+        writeLine();
+        boolean notAnswered = true;
 
+        while(notAnswered) {
+            System.out.println("Please choose an option");
+            System.out.println("(1) New game");
+            System.out.println("(2) Quit Program");
+            Scanner reader = new Scanner(System.in);
+            String input = reader.nextLine();
+            switch (input) {
+                case "1":
+                    notAnswered = false;
+                    break;
+                case "2":
+                    return;
+                default:
+                    System.out.println("Please enter 1 or 2");
+                    break;
+            }
+        }
+
+        writeLine();
+        System.out.println("Launch game");
+        Game game = new Game();
+        writeLine();
+        notAnswered = true;
+
+        while(notAnswered) {
+            System.out.println("Please choose an option");
+            System.out.println("(1) Accelerate");
+            System.out.println("(2) Decelerate");
+            System.out.println("(3) Next Segment");
+            Scanner reader = new Scanner(System.in);
+            String input = reader.nextLine();
+            switch (input) {
+                case "1":
+                    notAnswered = false;
+
+                    break;
+                case "2":
+                    return;
+                default:
+                    System.out.println("Please enter 1 or 2");
+                    break;
+            }
+        }
     }
 }
+
