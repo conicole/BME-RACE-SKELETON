ea1f3e432eb512375c1f33c0f5cc31fda88fd14d
conicole <corentin.nicol@live.com>
2015-04-08 16:29:08 +0200

    try to split config and game input treatment. Problem with matcher, and bufferreader isolation

--- src/prototype.java
+++ src/prototype.java
@@ -5,52 +5,99 @@ import java.util.regex.Pattern;
 
 
 public class prototype {
+
+    public static String[] cfgPatterns = {
+            "Launch",
+            "End",
+            "Save ([a-z]*)",
+            "Load ([a-z]*)",
+            "Add_Glue [0-9]",
+            "Add_Oil [0-9]",
+            "Add_Obstacle [0-9]",
+            "Add_Car [Position]",
+            "Add_Repair_Car [Position][0-9]",
+            "CreateGame ([0-9])",
+            "Add_Grass ([0-9])([0-9])",
+            "Create_Finish_Line ([0-9])([0-9])([0-9])([0-9])"
+    };
+
     Game game;
 
 
+    public prototype() {
+        game = new Game();
+    }
 
-    public static void main(String args[]) throws IOException
-    {
-        try{
+    // check if string s respect a pattern of a configuration command. Return the number of the command(-1). Else return -1
+    public static int isConfiguration(String s) {
+        int res = -1;
+        //System.out.println("let's check " + s);
+        for (int i = 0; i < cfgPatterns.length && res == -1; i++) {
+            Pattern pattern = Pattern.compile(cfgPatterns[i]);
+            Matcher matcher = pattern.matcher(s);
+           // System.out.println(cfgPatterns[i]);
+            if (matcher.find()) {
+                res = i;
+
+            }
+        }
+        return res;
+    }
+
+
+    public static void main(String args[]) throws IOException {
+        prototype proto = new prototype();
+        try {
             Game game = new Game();
 
-            // Open the file that is the first
-            // command line parameter
+            // Open the file
             FileInputStream fstream = new FileInputStream("input.txt");
             // Get the object of DataInputStream
             DataInputStream in = new DataInputStream(fstream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String strLine;
-            //Read File Line By Line
-            if( (strLine = br.readLine()) != null) {
-                String res = "";
-                String patternStr = "CreateGame ([0-9])";
-                Pattern pattern = Pattern.compile(patternStr);
-                Matcher matcher = pattern.matcher(strLine);
-                boolean matchFound = matcher.find();
-                System.out.println(matchFound);
-                if (matchFound && matcher.groupCount() >= 1) {
-                    res = matcher.group(1);
-                    for (int i = Integer.parseInt(res); i > 0; i--) {
-                        System.out.println("CAR CREATION");
-                    }
-                } else {
-                    return;
+
+            br.mark(100);
+            while ((strLine = br.readLine()) != null) {
+                if (isConfiguration(strLine) != -1) {
+                    proto.configuration(br);
                 }
+
+                in.close();
             }
-            else {
-                return;
-            }
-
-            while ((strLine = br.readLine()) != null)   {
-
-            }
-
-
-            //Close the input stream
-            in.close();
-        }catch (Exception e){//Catch exception if any
+        } catch (Exception e) {//Catch exception if any
             System.err.println("Error: " + e.getMessage());
         }
     }
+
+
+    public void configuration(BufferedReader br) throws java.io.IOException {
+        br.reset();
+        String strLine;
+        if ((strLine = br.readLine()) != null) {
+            int nbCmd = isConfiguration(strLine);
+            System.out.println(nbCmd);
+            this.ComputeCfgCmd(nbCmd);
+
+
+            // res = matcher.group(1);
+
+
+        }
+        br.reset();
+    }
+
+
+    public void ComputeCfgCmd(int nbCmd ){
+        System.out.println("cmd + "+ nbCmd);
+        switch(nbCmd) {
+            case 9:
+                System.out.println("Creation of game");
+                break;
+            default:
+                System.out.println("error");
+                break;
+        }
+    }
 }
+
