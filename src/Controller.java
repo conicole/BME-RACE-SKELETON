4690207445f28f6f26d0361ec0b880f8ea2f3dab
ljcbn <thomas08>
2015-05-04 11:26:35 +0200

    controller next

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,10 @@
 import com.sun.tools.javac.util.List;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
 import java.util.*;
+
 import javax.swing.JFrame;
 
 
@@ -10,10 +12,9 @@ public class Controller extends JFrame{
 	List<IView> views;
 	Game game;
 	TrackView track;
+	KeyboardInput keyboard = new KeyboardInput();
 
-	public Controller(Game g){
-		game = g;
-		KeyboardInput keyboard = new KeyboardInput();
+	public Controller(){
 		addKeyListener( keyboard );
 	}
 	
@@ -24,5 +25,41 @@ public class Controller extends JFrame{
 	public void setTrackView(TrackView t){
 		track=t;
 	}
+	
+	public void setGame(Game t){
+		game=t;
+	}
+	
+	public void run(){
+		keyboard.poll();
+		while(true){
+			if( keyboard.keyDownOnce( KeyEvent.VK_ESCAPE ) )  break;
+			processInput();
+			game.UpdateGame();
+		}
+	}
+	
+	 protected void processInput() {
+		 if( keyboard.keyDown( KeyEvent.VK_DOWN ) ){
+			 game.getCar(1).brake();
+		 }
+		 if( keyboard.keyDown( KeyEvent.VK_UP ) ){
+			 game.getCar(1).accelerate();
+		 }
+		 if( keyboard.keyDown( KeyEvent.VK_LEFT ) ){
+			 game.getCar(1).goLeft();
+		 }
+		 if( keyboard.keyDown( KeyEvent.VK_RIGHT ) ){
+			 game.getCar(1).goRight();
+		 }
+		 
+		 if( keyboard.keyDown( KeyEvent.VK_ALT ) ){
+			 game.getCar(1).releaseOil();
+		 }
+		 if( keyboard.keyDown( KeyEvent.VK_SHIFT ) ){
+			 game.getCar(1).releaseGlue();
+		 }
+		 
+	 }
 	
 }
